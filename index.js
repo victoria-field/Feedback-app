fe2927212ce77d6aeb7969dbccb43110d00fc2d5
victoria field <victoria.shadid@gmail.com>
2017-10-19 10:20:21 -0500

    finished auth flow

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session());
 
-require('./routes/authRoutes')(app);
+require('./routes/authroutes')(app);
 
 
 const PORT = process.env.PORT || 5000;
