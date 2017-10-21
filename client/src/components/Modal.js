ed1dbdc7b923cbc6f45ef28955b3d1ba810b68a3
victoria field <victoria.shadid@gmail.com>
2017-10-21 07:46:14 -0500

    changed images

--- client/src/components/Modal.js
+++ client/src/components/Modal.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 
+// require('./client/public/assets/img');
+
 class Modal extends Component {
 	constructor() {
 		super();
@@ -9,19 +11,27 @@ class Modal extends Component {
 			current_image: 1,
 			mood: '',
 			ok_images: [
-				'https://picsum.photos/300/300?image=2',
-				'https://picsum.photos/300/300?image=3',
-				'https://picsum.photos/300/300?image=1'
+				'https://picsum.photos/300/300?image=16',
+				'https://picsum.photos/300/300?image=81',
+				'https://picsum.photos/300/300?image=82'
 			],
 			neutral_images: [
-				'https://picsum.photos/300/300?image=5',
-				'https://picsum.photos/300/300?image=6',
-				'https://picsum.photos/300/300?image=7'
+				'https://picsum.photos/200/300?image=103',
+				'https://picsum.photos/200/300?image=106',
+				'https://picsum.photos/200/300?image=110'
 			],
 			anxious_images: [
-				'https://picsum.photos/300/300?image=8',
-				'https://picsum.photos/300/300?image=9',
-				'https://picsum.photos/300/300?image=10'
+				'https://picsum.photos/300/300?image=16',
+				'https://picsum.photos/300/300?image=17',
+				'https://picsum.photos/300/300?image=18',
+				'https://picsum.photos/300/300?image=53',
+				'https://picsum.photos/200/300?image=55',
+				'https://picsum.photos/200/300?image=57',
+				'https://picsum.photos/200/300?image=58',
+				'https://picsum.photos/200/300?image=76',
+				'https://picsum.photos/200/300?image=77',
+				'https://picsum.photos/200/300?image=79'
+
 			]
 		}
 	}
@@ -46,7 +56,7 @@ class Modal extends Component {
 	render() {
 		return(
 			<div className="modal column y-center">
-				<h3>Modal</h3>
+				<h3>How are you doing?</h3>
 				{!this.state.start ? (
 					<div className="choices">
 						<button onClick={() => this.startRelaxation('ok_images')}>Ok</button>
